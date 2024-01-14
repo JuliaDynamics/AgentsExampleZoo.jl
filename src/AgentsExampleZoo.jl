aa8b4a072dbb87401a9a6bae6abf53f949bb51f0
Tortar <68152031+Tortar@users.noreply.github.com>
2024-01-14 16:11:24 +0100

    comment out including statements to be able to release

--- src/AgentsExampleZoo.jl
+++ src/AgentsExampleZoo.jl
@@ -2,11 +2,12 @@
 module AgentsExampleZoo
 using Agents
 
-include("daisyworld.jl")
-include("flocking.jl")
-include("rabbit_fox_hawk.jl")
-include("schelling.jl")
-include("sir.jl")
-include("zombies.jl")
+# TODO: include all of these when we release Agents v6
+#include("daisyworld.jl")
+#include("flocking.jl")
+#include("rabbit_fox_hawk.jl")
+#include("schelling.jl")
+#include("sir.jl")
+#include("zombies.jl")
 
 end
