43b3ceeba4e455ad9f6b129f4017286397bce6ef
Tortar <68152031+Tortar@users.noreply.github.com>
2024-01-14 16:21:39 +0100

    Revert de-including the examples (#30)

--- src/AgentsExampleZoo.jl
+++ src/AgentsExampleZoo.jl
@@ -1,13 +1,14 @@
 
 module AgentsExampleZoo
-using Agents
 
-# TODO: include all of these when we release Agents v6
-#include("daisyworld.jl")
-#include("flocking.jl")
-#include("rabbit_fox_hawk.jl")
-#include("schelling.jl")
-#include("sir.jl")
-#include("zombies.jl")
+using Pkg
+Pkg.develop("Agents")
+
+include("daisyworld.jl")
+include("flocking.jl")
+include("rabbit_fox_hawk.jl")
+include("schelling.jl")
+include("sir.jl")
+include("zombies.jl")
 
 end
